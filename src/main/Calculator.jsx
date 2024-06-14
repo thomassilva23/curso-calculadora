7767a85dd29903fe011dcc0757d72bbca80ac69e
Thomas Silva <thomas@allto.digital>
2024-06-14 13:16:20 -0300

    4 - Implementando a Lógica #01

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,15 +3,26 @@ import "./Calculator.css";
 import Button from "../components/Button";
 import Display from "../components/Display";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class Calculator extends Component {
+  state = { ...initialState };
+
   constructor(props) {
     super(props);
     this.clearMemory = this.clearMemory.bind(this);
     this.setOperation = this.setOperation.bind(this);
     this.addDigit = this.addDigit.bind(this);
   }
+
   clearMemory() {
-    console.log("Limpar");
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
@@ -19,13 +30,30 @@ export default class Calculator extends Component {
   }
 
   addDigit(n) {
-    console.log(n);
+    // Regra para evitar que o usuário digite 2 '.'. Ex: 2.2.2
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100} />
+        <Display value={this.state.displayValue} />
         <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation />
         <Button label="7" click={this.addDigit} />
